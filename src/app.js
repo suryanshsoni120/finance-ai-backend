d7fafff0857b704de11d223694bc0f6ae1e97317
Suryansh Soni <surisoni10@gmail.com>
2026-01-15 21:59:38 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://myfinance-ai.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
